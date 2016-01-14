b842fba8f6e94ae518df954019021caf2dfa1cc1
Chris Talman <cewe.mail@gmail.com>
2016-01-14 13:42:38 +0000

    Update robot source code.

--- Functions/UpdateRobot.js
+++ Functions/UpdateRobot.js
@@ -2,5 +2,22 @@ module.exports = updateRobot;
 
 function updateRobot()
 {
+	spawnUpdater();
 	process.exit();
+};
+
+function spawnUpdater()
+{
+	const fs = require('fs');
+	const spawn = require('child_process').spawn;
+	const out = fs.openSync(global.appPath('Data/updateOutput.txt'), 'a');
+	const err = fs.openSync(global.appPath('Data/updateErrors.txt'), 'a');
+
+	const child = spawn('update.sh', [], {
+		cwd: __dirname,
+		detached: true,
+		stdio: ['ignore', out, err]
+	});
+
+	child.unref();
 };
