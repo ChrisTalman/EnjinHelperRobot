dbed398948dfaaf009816e711acd1c082f9c202a
Chris Talman <cewe.mail@gmail.com>
2016-01-15 18:03:55 +0000

    Request fix.

--- Functions/UpdateMember.js
+++ Functions/UpdateMember.js
@@ -65,7 +65,7 @@ function fulfilRoles(discordUser, callback, callbackArguments)
 						var enjinRequest = EnjinRequestTemplates.getUserTags;
 						enjinRequest.params.api_key = this.settings.enjin.api_key;
 						enjinRequest.params.user_id = enjinUserID;
-						Utilities.conductEnjinRequest.call(this, enjinRequest, 'fulfilRoles', function(dataJSON)
+						Utilities.conductEnjinRequest.call(this, enjinRequest, 'fulfilRoles', true, function(dataJSON, error)
 						{
 							var discordUserRoles = this.bot.servers[0].rolesOfUser(discordUser);
 							var enjinUserTags = dataJSON.result;
